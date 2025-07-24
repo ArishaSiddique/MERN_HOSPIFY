2670cdd39300f48cb6ebe2fa8e97354b2cc28a98
Shivani <adityashiva756@gmail.com>
2025-07-25 00:17:50 +0530

    Your commit message here

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@ import express from "express";
 import { config } from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import fileUpload  from "express-fileupload";
+import fileUpload from "express-fileupload";
 import { dbconnection } from "./database/dbconnection.js";
 import messageRouter from "./router/messageRouter.js";
-import {errorMiddleware} from "./middlewares/errorMiddleware.js";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 import userRouter from "./router/userRouter.js";
 import appointmentRouter from "./router/appointmentRouter.js";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
 
 
 const app = express();
@@ -16,14 +18,13 @@ config({ path: "./config/config.env" });
 //for connecting frontend with backend
 app.use(cors({
     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
-    methods:["GET","POST","PUT","DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
 
-
-app.use(cookieParser());//for using(get) cookies
-app.use(express.json());//passing data from json to string as we get data as in json format
-app.use(express.urlencoded({extended: true}));//for recognising data type ex-appointment request contain like 14 may
+app.use(express.json()); //passing data from json to string as we get data as in json format
+app.use(cookieParser()); //for using(get) cookies
+app.use(express.urlencoded({ extended: true })); //for recognising data type ex-appointment request contain like 14 may
 app.use(
     fileUpload({
         useTempFiles: true,
